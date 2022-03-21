3ff7293c696ee4ac7b55b0eb3fdec9e27e01bad8
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-21 19:58:13 -0400

    ci: testing

--- init.zsh
+++ init.zsh
@@ -25,6 +25,15 @@ p6df::modules::terraform::vscodes() {
     code --install-extension hashicorp.terraform
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::terraform::home::symlink()
+#
+#  Depends:	 p6_file
+#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
+#>
+######################################################################
 p6df::modules::terraform::home::symlink() {
 
     p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-terraform/share/.terraform.d" ".terraform.d"
