712edc48ff9b528a356d490b11cbcb2b8a7c2f4c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-05 23:25:16 -0400

    feat(terraform): add version lib helper (#39)

--- init.zsh
+++ init.zsh
@@ -20,7 +20,11 @@ p6df::modules::terraform::deps() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::terraform::env::init()
+# Function: p6df::modules::terraform::env::init(_module, _dir)
+#
+#  Args:
+#	_module -
+#	_dir -
 #
 #  Environment:	 TERRAFORM_BINARY_NAME
 #>
@@ -179,26 +183,4 @@ p6df::modules::terraform::prompt::context() {
     p6_return_str "$str"
 }
 
-######################################################################
-#<
-#
-# Function: str ver = p6_terraform_version()
-#
-#  Returns:
-#	str - ver
-#
-#  Environment:	 TERRAFORM_BINARY_NAME
-#>
-######################################################################
-p6_terraform_version() {
-
-  local ver
-  if p6_string_eq "tofu" "$TERRAFORM_BINARY_NAME"; then
-    ver=$(tofu --version | p6_filter_row_first 1 | p6_filter_column_pluck 2)
-  else
-    ver=$(terraform --version | p6_filter_row_first 1 | p6_filter_column_pluck 2)
-  fi
-
-  p6_return_str "$ver"
-}
 
